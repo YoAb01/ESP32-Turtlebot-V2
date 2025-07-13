12ab4be49c6929ef8bb2ea2742052c5ff24c82f5
YoAB01 <youssef.abl.learn.25@gmail.com>
2025-07-13 14:20:23 +0200

    Implemented IMU MPU6050 testing instance in main

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include "LED/LED.h"
 #include "Timer/Timer.h"
 #include "Motor/Motor.h"
+#include "IMU/IMU.h"
 
 #define BUILTIN_PIN   GPIO_NUM_2
 #define LED_R_PIN     GPIO_NUM_4
@@ -15,12 +16,15 @@
 #define MOTOR_B_IN2_PIN GPIO_NUM_41
 #define MOTOR_B_PWM_PIN GPIO_NUM_40
 
+#define I2C_MASTER_SCK_IO           GPIO_NUM_21
+#define I2C_MASTER_SDA_IO           GPIO_NUM_47
+
 LED builtin_led(BUILTIN_PIN);
 LED red_led(LED_R_PIN);
 LED green_led(LED_G_PIN);
 Motor motorA(MOTOR_A_IN1_PIN, MOTOR_A_IN2_PIN, MOTOR_A_PWM_PIN);
 Motor motorB(MOTOR_B_IN1_PIN, MOTOR_B_IN2_PIN, MOTOR_B_PWM_PIN);
-
+IMU mpu6050(I2C_MASTER_SDA_IO, I2C_MASTER_SCK_IO);
 
 void test_robot_motion(void *pvParameter) {
   while (1) {
@@ -46,6 +50,17 @@ void test_robot_motion(void *pvParameter) {
   }
 }
 
+void test_imu_data(void *pvParameter) {
+  while (1) {
+    double ax, ay, az, gx, gy, gz, tempC;
+    mpu6050.read_data(&ax, &ay, &az, &gx, &gy, &gz, &tempC);
+    printf("Accel (g): X=%.2f Y=%.2f Z=%.2f | Gyro (°/s): X=%.2f Y=%.2f Z=%.2f | Temp (°C): T=%.2f\n",
+               ax, ay, az, gx, gy, gz, tempC);
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
 extern "C" void app_main(void)
 {
 
@@ -61,7 +76,11 @@ extern "C" void app_main(void)
   motorA.init();
   motorB.init();
 
+  // IMU data
+  mpu6050.init_imu();
+
   xTaskCreate(test_robot_motion, "robot_movement_task", 2048, NULL, 5, NULL);
+  xTaskCreate(test_imu_data, "imu_data_task", 2048, NULL, 4, NULL);
 
 
 
